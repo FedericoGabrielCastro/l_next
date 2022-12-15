9a4a92c43225cb269be5f57f2157299f6dbb55bc
federico <fgc_castro@live.com>
2022-12-15 13:14:40 -0300

    show api data in page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
+import { Hydrate, QueryClientProvider } from 'react-query'
+import { queryClient } from '../src/api'
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps } ) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
+    </QueryClientProvider>
+  )
 }
 
 export default MyApp
